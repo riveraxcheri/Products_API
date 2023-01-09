329e7e2e4c4eb3cc7985157483ddefffe84c03cb
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2023-01-08 23:07:44 -0600

    Created Super User Admin + Added Products + Updated views/serializer/admin files

--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializer import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def products_list(request):
 
-    return Response('ok')
+    products = Product.objects.all()
+
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
