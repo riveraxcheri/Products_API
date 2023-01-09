e4b26e5c044ecf1833acc47ebbcf60132a067089
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2023-01-08 19:54:12 -0600

    Added .local_settings in port

--- products_project/settings.py
+++ products_project/settings.py
@@ -110,6 +110,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from products_project import *
+    from products_project.local_settings import *
 except ImportError:
     pass
